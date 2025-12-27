57ed27c35ffa6153c7f7940bd66847f812b59da3
Manoj <manojcpatil17@gmail.com>
2025-12-27 23:28:36 +0530

    Added few Validations, Added Custom Exceptions and GlobalExceptionHandler 4.1

--- lovable-clone/src/main/java/com/mp/projects/lovable_clone/error/GlobalExceptionHandler.java
+++ lovable-clone/src/main/java/com/mp/projects/lovable_clone/error/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.mp.projects.lovable_clone.error;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -21,6 +22,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiError> handleResourceNotFound(ResourceNotFoundException exception){
         ApiError apiError =new ApiError(HttpStatus.NOT_FOUND, exception.getResourceName() + " with ID " + exception.getResourceId() + " not found.");
         log.error(apiError.toString(), exception);
+        return ResponseEntity.status(apiError.status()).body(apiError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleInputValidationError(MethodArgumentNotValidException exception){
+        ApiError apiError=new ApiError(HttpStatus.BAD_REQUEST,exception.getLocalizedMessage());
+        log.error(apiError.toString(),exception);
         return ResponseEntity.status(apiError.status()).body(apiError);
     }
 }
